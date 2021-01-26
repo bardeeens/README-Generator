80025c7c20e95c206953f92157675f365b2f3502
Bardini <derekbardini@gmail.com>
2021-01-25 23:28:45 -0600

    added quite a bit of functionality.

--- Develop/README1.md
+++ Develop/README1.md
@@ -1,1 +1,29 @@
-# Super Dope README Generator
+# **poop daily** 
+## Description:
+we all get together and talk about poop 
+Table of Contents: 
+* Installation 
+* Usage 
+* License 
+* Contributors 
+* Tests 
+* Questions
+
+      ### Installation 
+anally 
+### Usage 
+well 
+### License 
+### Contributors 
+not me 
+
+      ### Tests 
+yeah in the butt 
+### Questions? 
+ You can find the github repository here: 
+butts.com 
+ Feel free to contact me at butts25@aol.com if you have any questions or comments!
+    
+      
+      
+      
--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,7 @@
+const generate = require('./utils/generateMarkdown.js')
 const inquirer = require('inquirer');
 const fs = require('fs');
+
 
 inquirer
   .prompt([
@@ -9,14 +11,45 @@ inquirer
       name: 'title',
     },
     {
-      type: 'password',
-      message: 'What is your password?',
-      name: 'password',
+      type: 'input',
+      message: 'Please give a brief description of the program.',
+      name: 'description',
     },
     {
-      type: 'password',
-      message: 'Re-enter password to confirm:',
-      name: 'confirm',
+      type: 'input',
+      message: 'How is this programmed installed?',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'How is this program used?',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'Who are the contributors?',
+      name: 'contributors',
+    },
+    {
+      type: 'input',
+      message: 'Are there any tests for ensuring that it is working?',
+      name: 'tests',
+    },
+    {
+      type: 'input',
+      message: 'What the repository url?',
+      name: 'github',
+    },
+    {
+      type: 'input',
+      message: 'What is your email address?',
+      name: 'email',
+    },
+    {
+      type: 'list',
+      message: 'Which license would you like to use?',
+      choices: ["WTFPL", "Unlicense"],
+      name: 'license',
     },
   ])
   
@@ -26,13 +59,46 @@ inquirer
     //   : console.log('You forgot your password already?!')
     .then((response) =>    
       {
-        console.log(response.title);
-      let README = `# ${response.title}`
+     
+        let licenseURL = ``
+        console.log(response.license);
+        // console.log(license === "WTFPL");
+        switch (response.license) {
+            case "WTFPL":
+            licenseURL = `[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)`;
+            console.log("wtfbro");  
+                break;
+            case "Unlicense":
+            licenseURL = `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
+            console.log("Unlicense my bro");
+                break;
+            default:
+                console.log("didn't work");
+        }
+
+            // console.log(license);
+
+
+      let README = `# **${response.title}** \n## Description:\n${response.description} \nTable of Contents: \n* Installation \n* Usage \n* License \n* Contributors \n* Tests \n* Questions
+
+      ### Installation \n${response.installation} \n### Usage \n${response.usage} \n### License \n### Contributors \n${response.contributors} \n
+      ### Tests \n${response.tests} \n### Questions? \n You can find the github repository here: \n${response.github} \n Feel free to contact me at ${response.email} if you have any questions or comments!
+    
+      
+      
+      `
       
       fs.writeFile("README1.md", README, (error) => {
         if (error){
           console.log(error);
+        } else {
+            console.log("README successfully generated!");
         }
       })
       }
   );
+
+  function init(){
+      generate.generateMarkdown();
+  }
+  init();
