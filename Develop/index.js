37183479ab427ef8f5ddec683d8497cc4e313618
Bardini <derekbardini@gmail.com>
2021-01-26 18:29:56 -0600

    everything is working!

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,7 @@
-const generate = require('./utils/generateMarkdown.js')
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-
-inquirer
-  .prompt([
+const questions = [
     {
       type: 'input',
       message: 'What is the title of your project?',
@@ -51,38 +48,80 @@ inquirer
       choices: ["WTFPL", "Unlicense"],
       name: 'license',
     },
-  ])
+  ];
+
+inquirer
+  .prompt(questions)
+
   
  
-    // response.confirm === response.password
-    //   ? console.log('Success!')
-    //   : console.log('You forgot your password already?!')
+    
     .then((response) =>    
       {
      
         let licenseURL = ``
+        let licensestatement = ``
         console.log(response.license);
-        // console.log(license === "WTFPL");
         switch (response.license) {
             case "WTFPL":
             licenseURL = `[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)`;
+            licensestatement =         
+    ` DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+    Version 2, December 2004
+
+    Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+
+    Everyone is permitted to copy and distribute verbatim or modified
+    copies of this license document, and changing it is allowed as long
+    as the name is changed.
+
+    DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+    TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+    0. You just DO WHAT THE FUCK YOU WANT TO.`
             console.log("wtfbro");  
                 break;
             case "Unlicense":
             licenseURL = `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
+            licensestatement =
+    ` This is free and unencumbered software released into the public domain.
+
+    Anyone is free to copy, modify, publish, use, compile, sell, or
+    distribute this software, either in source code form or as a compiled
+    binary, for any purpose, commercial or non-commercial, and by any
+    means.
+    
+    In jurisdictions that recognize copyright laws, the author or authors
+    of this software dedicate any and all copyright interest in the
+    software to the public domain. We make this dedication for the benefit
+    of the public at large and to the detriment of our heirs and
+    successors. We intend this dedication to be an overt act of
+    relinquishment in perpetuity of all present and future rights to this
+    software under copyright law.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+    OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+    
+    For more information, please refer to <http://unlicense.org/>`
+
             console.log("Unlicense my bro");
                 break;
             default:
                 console.log("didn't work");
         }
 
-            // console.log(license);
 
-
-      let README = `# **${response.title}** \n## Description:\n${response.description} \nTable of Contents: \n* Installation \n* Usage \n* License \n* Contributors \n* Tests \n* Questions
-
-      ### Installation \n${response.installation} \n### Usage \n${response.usage} \n### License \n### Contributors \n${response.contributors} \n
-      ### Tests \n${response.tests} \n### Questions? \n You can find the github repository here: \n${response.github} \n Feel free to contact me at ${response.email} if you have any questions or comments!
+      let README = 
+`# **${response.title}** \n${licenseURL} \n## Description:\n${response.description} \n  ## Table of Contents: \n* Installation 
+\n* Usage \n* Contributors \n* Tests \n* Questions \n* License \n### Installation \n${response.installation} \n### Usage \n${response.usage} 
+\n### License \n### Contributors \n${response.contributors} \n### Tests \n${response.tests} \n### Questions? 
+\n You can find the github repository here: \n${response.github} \n Feel free to contact me at ${response.email} if you have any questions or comments! 
+\n### Licensing \n   ${licensestatement}
     
       
       
@@ -98,7 +137,4 @@ inquirer
       }
   );
 
-  function init(){
-      generate.generateMarkdown();
-  }
-  init();
+ 
